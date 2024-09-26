6e4b7cfe50db9f33cd8fed2c2e2e9d5d0243d9e2
Kirill Lalayants <lalayantskirill@gmail.com>
2024-09-27 00:28:48 +0300

    fix 8

--- 8/init.m
+++ 8/init.m
@@ -12,11 +12,11 @@ Lambda = -k_0;
 gamma = 10000;
 is_zero = true;
 gammas = [1, 100, 10000];
-[Ass, Bss, Css, Dss] = tf2ss(b_0,[a_1,a_0]);
+[Ass, Bss, Css, Dss] = tf2ss(b_0,[1, a_1, a_0]);
 init_ss = 1;
 for gamma = gammas
    display(gamma);
-   out = sim('lab8.slx', 100000);
+   out = sim('lab8.slx', 10000);
    csvwrite('0_' + string(gamma) + '_time.csv', out.y_m.Time);
    csvwrite('0_' + string(gamma) + '_epsilon.csv', out.y_m.Data - out.y.Data);
    % csvwrite('0_' + string(gamma) + '_y.csv', out.y.Data);
@@ -27,7 +27,7 @@ end
 is_zero = false;
 for gamma = gammas
    display(gamma);
-   out = sim('lab8.slx', 100000);
+   out = sim('lab8.slx', 10000);
    csvwrite('1_' + string(gamma) + '_time.csv', out.y_m.Time);
    csvwrite('1_' + string(gamma) + '_epsilon.csv', out.y_m.Data - out.y.Data);
    % csvwrite('1_' + string(gamma) + '_y.csv', out.y.Data);
